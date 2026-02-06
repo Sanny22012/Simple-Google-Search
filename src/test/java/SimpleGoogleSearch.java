a4e3d2c25df8414d34d3b9f2b1e4aee4c183b074
Sanny Anand <sanny12012@gmail.com>
2026-02-06 22:48:12 +0530

    ssd

--- src/test/java/SimpleGoogleSearch.java
+++ src/test/java/SimpleGoogleSearch.java
@@ -4,34 +4,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SimpleGoogleSearch {
 
-    WebDriver driver;
+	WebDriver driver;
 
-    @BeforeClass
-    public void setup() {
-        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe"); // your ChromeDriver path
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+	@BeforeMethod
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
 
-    @Test
-    public void searchGoogle() throws InterruptedException {
-        driver.get("https://www.google.com");
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium WebDriver" + Keys.ENTER);
-        Thread.sleep(2000); // simple wait for results
-        String title = driver.getTitle();
-        Assert.assertTrue(title.contains("Selenium WebDriver"), "Search failed!");
-        System.out.println("Search successful: " + title);
-    }
+	@Test
+	public void searchGoogle() throws InterruptedException {
+		driver.get("https://www.google.com");
+		WebElement searchBox = driver.findElement(By.name("q"));
+		searchBox.sendKeys("Selenium WebDriver" + Keys.ENTER);
+		Thread.sleep(2000); // simple wait for results
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Selenium WebDriver"), "Search failed!");
+		System.out.println("Search successful: " + title);
+	}
 
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) driver.quit();
-    }
+	@AfterMethod
+	public void tearDown() {
+		if (driver != null)
+			driver.quit();
+	}
 }
--- src/test/java/restassuredTest/GetEwquest.java
+++ src/test/java/restassuredTest/GetEwquest.java
@@ -1,32 +1,32 @@
-package restassuredTest;
-
-import org.testng.annotations.Test;
-
-import io.restassured.matcher.ResponseAwareMatcher;
-import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.*;
-import org.testng.annotations.Test;
-
-
-public class GetEwquest {
-
-	@Test
-	public void getWeatherDetails() {
-		given()
-		.when()
-			.get("http://restapi.demoqa.com/utilities/weather/city/Hyderabad")
-		.then()
-		.statusCode(200)
-		.statusLine("HTTP/1.1 200 OK")
-		.assertThat().body("City",equalTo("Hyderabad"))
-		.header("Content-Tupe", "appilcation/json");
-	}
-
-	private ResponseAwareMatcher<Response> equalTo(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-}
+//package restassuredTest;
+//
+//import org.testng.annotations.Test;
+//
+//import io.restassured.matcher.ResponseAwareMatcher;
+//import io.restassured.response.Response;
+//
+//import static io.restassured.RestAssured.*;
+//import org.testng.annotations.Test;
+//
+//
+//public class GetEwquest {
+//
+//	@Test
+//	public void getWeatherDetails() {
+//		given()
+//		.when()
+//			.get("http://restapi.demoqa.com/utilities/weather/city/Hyderabad")
+//		.then()
+//		.statusCode(200)
+//		.statusLine("HTTP/1.1 200 OK")
+//		.assertThat().body("City",equalTo("Hyderabad"))
+//		.header("Content-Tupe", "appilcation/json");
+//	}
+//
+//	private ResponseAwareMatcher<Response> equalTo(String string) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	
+//	
+//}
